35a984d58817b9bc5673857d0355c278bb37e6e7
Sylvain Blondeau <sylvain.blondeau@wildcodeschool.com>
2021-12-21 15:51:36 +0100

    member touits

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -18,15 +18,4 @@ class HomeController extends AbstractController
         $touits = $touitRepository->findAll();
         return $this->render('home/index.html.twig', ['touits' => $touits]);
     }
-
-    /**
-     * @Route("/mon-fil", name="my_touit", methods={"GET"})
-     * @isGranted("ROLE_USER");
-     */
-    public function myTouit(TouitRepository $touitRepository): Response
-    {
-        $touits = $touitRepository->findByUser($this->getUser());
-
-        return $this->render('home/index.html.twig', ['touits' => $touits]);
-    }
 }
--- src/Controller/MemberController.php
+++ src/Controller/MemberController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 class MemberController extends AbstractController
 {
@@ -17,5 +19,14 @@ class MemberController extends AbstractController
         return $this->render('member/index.html.twig', [
             'members' => $userRepository->findBy([], ['username' => 'ASC']),
         ]);
+    }
+
+    /**
+     * @Route("/member/{user}", name="member_touits", methods={"GET"})
+     * @isGranted("ROLE_USER");
+     */
+    public function memberTouits(User $user): Response
+    {
+        return $this->render('member/showTouits.html.twig', ['member' => $user]);
     }
 }
